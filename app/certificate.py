d5fe69219a0651feb801cee9d54cf51ed69bc7db
nezlobin118@gmail.com <nezlobin118@gmail.com>
2022-02-13 16:34:10 +0800

    1

--- app/certificate.py
+++ app/certificate.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from PIL import Image, ImageDraw, ImageFont
-from slugify import slugify
 from config import Config
 from textwrap import fill
 import qrcode
@@ -14,18 +13,16 @@ def cert(form):
     except OSError as e:
         print(e)
     img = Image.open('app/template.png')
-    font = ImageFont.truetype(f"app/font/DISTANT_STROKE_MEDIUM.OTF", size=350)
+    font = ImageFont.truetype(f"app/font/Shelley_Volante.ttf", size=300)
     idraw = ImageDraw.Draw(img)
     MAX_W, MAX_H = img.size
-    text = f"{slugify(form.first_name, lowercase=False)} {slugify(form.last_name, lowercase=False)}"
+    text = f"{form.first_name} {form.last_name}"
     w, h = idraw.textsize(text, font=font)
     x = (MAX_W - w) / 2
     y = (MAX_H - h) / 2
     idraw.text((x, y + 170), text, (49, 52, 76), font=font, align="center")
-    text = fill(
-        f"This certificate confirms that a {slugify(form.last_name, lowercase=False)} {slugify(form.first_name, lowercase=False)} has the right to own planet {slugify(form.name_cosmic, lowercase=False)}",
-        70)
-    font = ImageFont.truetype(f"app/font/DISTANT_STROKE.OTF", size=120)
+    text = fill(f"Имеет право владения планетой {form.name_cosmic}", 45)
+    font = ImageFont.truetype(f"app/font/Shelley_Volante.ttf", size=100)
     w, h = idraw.textsize(text, font=font)
     x = (MAX_W - w) / 2
     y = (MAX_H - h) / 2
